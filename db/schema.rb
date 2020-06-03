b8baf1462bef733bdd2d1cc9f2b03f7100666aaa
Zachariah Chow <zachariahchow@gmail.com>
2020-06-03 18:58:37 +0800

    feat: Run create cart table migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_03_105509) do
+ActiveRecord::Schema.define(version: 2020_06_03_105803) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "image_url"
@@ -35,4 +42,5 @@ ActiveRecord::Schema.define(version: 2020_06_03_105509) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "carts", "users"
 end

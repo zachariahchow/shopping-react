aa204c816a50faf7c299df5e525301aae4405d77
Zachariah Chow <zachariahchow@gmail.com>
2020-06-03 23:15:37 +0800

    feat: Add Cart Component

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -2,16 +2,17 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import SearchInput from './Search/SearchInput';
 import SearchResults from './Search/SearchResults';
+import Cart from './Cart/Cart';
 
 const App = () => {
 
     //States
 
     const [productList, setProductList] = useState();
-
     const [searchedProducts, setSearchedProducts] = useState([]);
-
     const [searchInputValue, setSearchInputValue] = useState(null);
+    const [cartItems, setCartItems] = useState([]);
+
     //
 
     const getProducts = async () => {
@@ -53,10 +54,31 @@ const App = () => {
         console.log(searchedProducts);
     }, [searchedProducts]);
 
+    //
+
+    const addToCartClickHandler = (ev) => {
+        // const productId = ev.target.dataset.productId;
+        const addedProduct = productList
+            .find(prod =>
+                prod.id == ev.target.dataset.productId
+            );
+
+        console.log(addedProduct);
+
+        if (!cartItems.find(prod => prod.id == addedProduct.id)) {
+            setCartItems(cartItems.concat(addedProduct));
+        }
+    }
+
+    useEffect(() => {
+        console.log(cartItems);
+    }, [cartItems]);
+
     return (
         <main>
             <SearchInput changeHandler={searchInputChangeHandler} clickHandler={submitBtnClickHandler}/>
-            {productList ? <SearchResults products={productList}/> : null}
+            <Cart products={cartItems}/>
+            {productList ? <SearchResults products={productList} clickHandler={addToCartClickHandler}/> : null}
         </main>
     );
 }
--- app/javascript/components/Cart/Cart.jsx
+++ app/javascript/components/Cart/Cart.jsx
@@ -1,25 +1,27 @@
 import React from 'react';
 
-const SearchResults = ({ products, clickHandler }) => {
+const Cart = ({ products, clickHandler }) => {
 
-    const productEls = products.map(prod => {
+    const cartItems = products.map(prod => {
         return (
-            <div className="product__container">
+            <div className="cart-product__container">
                 <div className="product-image__container">
                     <img src={prod.image_url} alt="" className="product-image"/>
                 </div>
                 <h3 className="product-name">Item: {prod.name}</h3>
                 <h3 className="product-price">Price: {prod.price}</h3>
                 <h4 className="product-description">Description: {prod.description}</h4>
-                <button className="product-add-cart__btn" data-product-id={prod.id} onClick={clickHandler}>Add to Cart</button>
+                <button className="product-delete-cart__btn" data-product-id={prod.id} onClick={clickHandler}>Remove from Cart</button>
             </div>
         )
     })
+
     return (
-        <div className="search-results__container">
-            {productEls}
+        <div className="cart__container">
+            <h2 className="cart__header">Cart</h2>
+            {cartItems}
         </div>
     );
 }
 
-export default SearchResults;
+export default Cart;
